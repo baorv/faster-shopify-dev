1abd2713afec81df750c5a22627ce468cc9cd67f
Bob Roan <roanvanbao@gmail.com>
2019-10-11 17:32:19 +0700

    Don't check node version

--- bin/helpers.js
+++ bin/helpers.js
@@ -22,25 +22,3 @@ exports.isPortOpen = function(port) {
       .listen(port);
   });
 };
-
-/**
- * Check node version for allowing to enter to the application
- *
- * @param {string} version
- */
-exports.checkNodeVersion = function(version) {
-  const currentNodeVersion = process.versions.node;
-  const supportedSemver = version.split('.');
-  const currentSemver = currentNodeVersion.split('.');
-
-  if (
-    currentSemver[0] < supportedSemver[0] ||
-    (currentSemver[0] === supportedSemver[0] &&
-      currentSemver[1] < supportedSemver[1])
-  ) {
-    console.error(
-      `You are running Node ${currentNodeVersion}. Please update your version of node to ${version} or higher to continue.`
-    );
-    process.exit(1);
-  }
-};
